17baa317f05b8287d8a46b0a90b75047987f0c2f
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-05 15:13:30 -0700

    env add

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8889/products_db'
+app.config['SQLALCHEMY_DATABASE_URI'] =os.getenv("SQL_CONECTION")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
