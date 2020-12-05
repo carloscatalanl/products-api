b1361e410f3484acd0f27eaa4e7e69a853af7095
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-04 17:09:26 -0700

    product API CRUD

--- src/app.py
+++ src/app.py
@@ -1,56 +1,92 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8889/products_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from tasks import tasks
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-# List Tasks
-@app.route('/tasks')
-def getTasks():
-    return jsonify({'tasks': tasks})
+# Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70))
+    price = db.Column(db.Integer)
+    description = db.Column(db.String(100))
 
-@app.route('/tasks/<int:task_id>')
-def getTask(task_id):
-    tasksFound = [
-        task for task in tasks if task['id'] == task_id]
-    if (len(tasksFound) > 0):
-        return jsonify({'task': tasksFound[0]})
-    return jsonify({'message': 'Task Not found'})
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
 
-# Create New Task
-@app.route('/tasks', methods=['POST'])
-def addTask():
-    new_task = {
-        'name': request.json['name'],
-        'description': request.json['description']
-    }
-    tasks.append(new_task)
-    return jsonify({'tasks': tasks})
+db.create_all()
 
-# Update Task
-@app.route('/tasks/<int:task_id>', methods=['PUT'])
-def editTask(task_id):
-    tasksFound = [task for task in tasks if task['id'] == task_id]
-    if (len(tasksFound) > 0):
-        tasksFound[0]['name'] = request.json['name']
-        tasksFound[0]['description'] = request.json['description']
-        return jsonify({
-            'message': 'Task Updated!',
-            'task': tasksFound[0]
-        })
-    return jsonify({'message': 'Task Not found'})
+# Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'price', 'description')
 
-# Delete Task
-@app.route('/tasks/<int:task_id>', methods=['DELETE'])
-def deleteTask(task_id):
-    tasksFound = [task for task in tasks if task['id'] == task_id]
-    if len(tasksFound) > 0:
-        tasks.remove(tasksFound[0])
-        return jsonify({
-            'message': 'Task Deleted!',
-            'tasks': tasks
-        })
-    return jsonify({'message': 'Task Not found'})
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+# Create product
+@app.route('/products', methods=['Post'])
+def create_product():
+  name = request.json['name']
+  price = request.json['price']
+  description = request.json['description']
+
+  new_product= Product(name, price, description)
+
+  db.session.add(new_product)
+  db.session.commit()
+
+  return product_schema.jsonify(new_product)
+
+# List all products
+@app.route('/products', methods=['GET'])
+def get_products():
+  all_products = Product.query.all()
+  result = products_schema.dump(all_products)
+  return jsonify(result)
+
+# List single product
+@app.route('/products/<id>', methods=['GET'])
+def get_product(id):
+  product = Product.query.get(id)
+  return product_schema.jsonify(product)
+
+# Update product
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+  product = Product.query.get(id)
+
+  name = request.json['name']
+  price = request.json['price']
+  description = request.json['description']
+
+  product.name = name
+  product.price = price
+  product.description = description
+
+  db.session.commit()
+
+  return product_schema.jsonify(product)
+
+# Delete product
+@app.route('/products/<id>', methods=['DELETE'])
+def delete_product(id):
+  product = Product.query.get(id)
+  db.session.delete(product)
+  db.session.commit()
+  return product_schema.jsonify(product)
+
+# Index
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Flask API Ready'})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=7000, debug=True)
--- src/tasks.py
+++
@@ -1,5 +0,0 @@
-tasks = [
-    {'id':1,'name': 'Clean', 'description': 'Clean...'},
-    {'id':2,'name': 'Another Task', 'description': 'Just another task...'},
-    {'id':3,'name': 'Study', 'description': 'Study Devops Pipelines...'}
-]
